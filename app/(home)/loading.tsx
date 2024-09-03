a2bdbffd61a08fbed52cfb44774eade52b7cc02b
HyunJongLee <dlguswhd1009@naver.com>
2024-09-03 20:50:19 +0900

    loading page updated

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -1,9 +1,22 @@
-import styles from '../../css/loading.module.css'
+import styles from '../../css/home.module.css';
 
-export default function AWG(){
-  return(
-    <h1 className={styles.loading}>
-      isLoading...
-    </h1>
+export default function AWG() {
+  const test = [];
+  for (let i = 0; i < 20; i++) {
+    test.push(i);
+  }
+  return (
+    <div className={styles.wrapper}>
+      {test.map((_, idx) =>
+        <div
+          key={idx}
+          style={
+            {
+              height: '500px',
+              background: 'linear-gradient(180deg, #8e9eab, #eef2f3)',
+              borderRadius: '20px',
+            }}>
+        </div>)}
+    </div>
   );
 }
